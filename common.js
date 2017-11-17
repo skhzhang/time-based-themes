2bdaa9d9b49d2205b96d1469ddc3fd08054be504
Simon Zhang <simonkhzhang@gmail.com>
2017-11-17 17:55:35 -0500

    - Set themes using user installed themes (Using browser.management API). - Add default values for localStorage values.

--- common.js
+++ common.js
@@ -1,21 +1,29 @@
 const KEY_PREFIX = 'tbthemesinfo';
 
-var currentTheme = '';
-const currentThemeKey = KEY_PREFIX + currentTheme;
+const daytimeThemeKey = KEY_PREFIX + "daytimeTheme";
+const nighttimeThemeKey = KEY_PREFIX + "nighttimeTheme";
 const checkTimeIntervalKey = KEY_PREFIX + "checkTimeInterval";
 const sunriseTimeKey = KEY_PREFIX + "sunriseTime";
 const sunsetTimeKey = KEY_PREFIX + "sunsetTime";
 
-// Set interval to 5 if it has never been set before,
+let DEFAULT_DAYTIME_THEME = "";
+let DEFAULT_NIGHTTIME_THEME = "";
+const DEFAULT_CHECK_TIME_INTERVAL = 5;
+const DEFAULT_SUNRISE_TIME = "08:00";
+const DEFAULT_SUNSET_TIME = "20:00";
+
+// var currentTheme = '';
+
+// Set values if they each have never been set before,
 // such as on first-time startup.
 if (!localStorage.hasOwnProperty(checkTimeIntervalKey)) {
-  localStorage[checkTimeIntervalKey] = 5;
+    localStorage[checkTimeIntervalKey] = DEFAULT_CHECK_TIME_INTERVAL;
 }
 if (!localStorage.hasOwnProperty(sunriseTimeKey)) {
-  localStorage[sunriseTimeKey] = '08:00';
+    localStorage[sunriseTimeKey] = DEFAULT_SUNRISE_TIME;
 }
 if (!localStorage.hasOwnProperty(sunsetTimeKey)) {
-  localStorage[sunsetTimeKey] = "20:00";
+    localStorage[sunsetTimeKey] = DEFAULT_SUNSET_TIME;
 }
 
 const themes = {
@@ -61,83 +69,97 @@ const themes = {
   }
 };
 
+/*
 // Set the theme.
 function setTheme(theme) {
-  if (currentTheme === theme) {
+  if (theme === currentTheme) {
     // No point in changing the theme if it has already been set.
     return;
   }
+  else if (theme === "default") {
+    resetTheme();
+  }
+  else {
+    console.log("Set theme to " + theme);
+    console.log(themes[theme]);
 
-  console.log("Set theme to " + theme);
-  console.log(themes[theme]);
+    currentTheme = theme;
+    browser.theme.update(themes[theme]);
+  }
+}
+*/
 
-  currentTheme = theme;
-  browser.theme.update(themes[theme]);
+// Enable the theme.
+function enableTheme(themeId) {
+    browser.management.setEnabled(themeId, true);
+    console.log("Enabled theme " + themeId);
 }
 
 // Reset all updates made to the user's theme.
 function resetTheme() {
-  browser.theme.reset();
-  console.log("Reset the user's theme to their originally selected theme.");
+    browser.theme.reset();
+    console.log("Reset the user's theme to their originally selected theme.");
 }
 
 // Check the current system time and set the theme based on the time.
 function checkTime() {
-  let date = new Date();
-  let hours = date.getHours();
-  let minutes = date.getMinutes();
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
 
-  let sunriseHours = localStorage[sunriseTimeKey].split(":")[0];
-  let sunriseMins = localStorage[sunriseTimeKey].split(":")[1];
-  let sunsetHours = localStorage[sunsetTimeKey].split(":")[0];
-  let sunsetMins = localStorage[sunsetTimeKey].split(":")[1];
+    let sunriseSplit = localStorage[sunriseTimeKey].split(":");
+    let sunsetSplit = localStorage[sunsetTimeKey].split(":");
 
-  // Will set the sun theme between sunrise and sunset.
-  if (timeInBetween(hours, minutes, sunriseHours, sunriseMins, sunsetHours, sunsetMins)) {
-    setTheme('light');
-  } else {
-    setTheme('dark');
-  }
+    // Will set the sun theme between sunrise and sunset.
+    if (timeInBetween(hours, minutes, sunriseSplit[0], sunriseSplit[1], sunsetSplit[0], sunsetSplit[1])) {
+        console.log(localStorage[daytimeThemeKey]);
+        //setTheme(localStorage[daytimeThemeKey]);
+        enableTheme(localStorage[daytimeThemeKey]);
+    } else {
+        console.log(localStorage[nighttimeThemeKey]);
+        //setTheme(localStorage[nighttimeThemeKey]);
+        enableTheme(localStorage[nighttimeThemeKey]);
+    }
 }
 
 // Update the checkTime alarm.
 function updateCheckTime(timeInterval) {
-  browser.alarms.onAlarm.removeListener(checkTime);
-  browser.alarms.onAlarm.addListener(checkTime);
-  browser.alarms.create('checkTime', {periodInMinutes: parseInt(localStorage[checkTimeIntervalKey])});
-  console.log("Set the alarm interval time to " + localStorage[checkTimeIntervalKey] + " mins.");
+    browser.alarms.onAlarm.removeListener(checkTime);
+    browser.alarms.onAlarm.addListener(checkTime);
+    browser.alarms.create('checkTime', {periodInMinutes: parseInt(localStorage[checkTimeIntervalKey])});
+    console.log("Set the alarm interval time to " + localStorage[checkTimeIntervalKey] + " mins.");
 }
 
 // Helper function to figure out if a time is in-between two times.
 function timeInBetween(
-    curHours, curMins, 
-    sunriseHours, sunriseMins, 
-    sunsetHours, sunsetMins
-  ) {
-  var curTimeInMins = curHours * 60 + parseInt(curMins);
-  var sunriseInMins = sunriseHours * 60 + parseInt(sunriseMins);
-  var sunsetInMins = sunsetHours * 60 + parseInt(sunsetMins);
-  var difference = sunsetInMins - sunriseInMins;
+      curHours, curMins, 
+      sunriseHours, sunriseMins, 
+      sunsetHours, sunsetMins
+    ) {
+    let curTimeInMins = curHours * 60 + parseInt(curMins);
+    let sunriseInMins = sunriseHours * 60 + parseInt(sunriseMins);
+    let sunsetInMins = sunsetHours * 60 + parseInt(sunsetMins);
+    let difference = sunsetInMins - sunriseInMins;
 
-  // If the difference is negative, 
-  // we know the "sunrise" is on the previous day.
-  if (difference < 0) {
-    difference += 1440;
+    // If the difference is negative, 
+    // we know the "sunrise" is on the previous day.
+    if (difference < 0) {
+        difference += 1440;
 
-    // So, we need to do the comparisons a little differently.
-    if (sunriseInMins <= curTimeInMins || curTimeInMins < sunsetInMins) {
-      console.log("It is currently daytime.");
-      return true;
+        // So, we need to do the comparisons a little differently.
+        if (sunriseInMins <= curTimeInMins || curTimeInMins < sunsetInMins) {
+            console.log("It is currently daytime.");
+            return true;
+        }
     }
-  }
-  else {
-    if (sunriseInMins <= curTimeInMins && curTimeInMins < sunsetInMins) {
-      console.log("It is currently daytime.");
-      return true;
+    else {
+        if (sunriseInMins <= curTimeInMins && curTimeInMins < sunsetInMins) {
+            console.log("It is currently daytime.");
+            return true;
+        }
     }
-  }
 
-  console.log("It is currently nighttime.");
+    console.log("It is currently nighttime.");
 
-  return false;
+    return false;
 }
--- options.js
+++ options.js
@@ -1,62 +1,112 @@
-var alarmIntervalInput = document.getElementById("alarm-interval");
-var sunriseInput = document.getElementById("sunrise-time");
-var sunsetInput = document.getElementById("sunset-time");
+let checkTimeBtn = document.getElementById("check-time-btn");
+let alarmIntervalInput = document.getElementById("alarm-interval");
+let sunriseInput = document.getElementById("sunrise-time");
+let sunsetInput = document.getElementById("sunset-time");
+let daytimeThemeList = document.getElementById("daytime-theme-list");
+let nighttimeThemeList = document.getElementById("nighttime-theme-list");
+let resetDefaultBtn = document.getElementById("reset-default-btn");
+
+// Iterate through each extension.
+browser.management.getAll().then((extensions) => {
+    for (let extension of extensions) {
+        if (extension.type === 'theme') {
+            let extOption = document.createElement('option');
+            extOption.textContent = extension.name;
+            extOption.value = extension.id;
+            if (extension.enabled) {
+                extOption.selected = true;
+            }
+
+            // Add each theme as an option in the dropdowns.
+            daytimeThemeList.appendChild(extOption);
+            nighttimeThemeList.appendChild(extOption.cloneNode(true));
+
+            // Set the default based on what theme is
+            // named default/dark.
+            // TODO: Find a better way to do this.
+            if (extension.name === "Default") {
+                DEFAULT_DAYTIME_THEME = extension.id;
+            }
+            if (extension.name === "Dark") {
+                DEFAULT_NIGHTTIME_THEME = extension.id;
+            }
+        }
+    }
+
+    // Set values if they each have never been set before,
+    // such as on first-time startup.
+    if (!localStorage.hasOwnProperty(daytimeThemeKey)) {
+      localStorage[daytimeThemeKey] = DEFAULT_DAYTIME_THEME;
+    }
+    if (!localStorage.hasOwnProperty(nighttimeThemeKey)) {
+      localStorage[nighttimeThemeKey] = DEFAULT_NIGHTTIME_THEME;
+    }
+
+    // Set the value to match the one in localStorage.
+    daytimeThemeList.value = localStorage[daytimeThemeKey];
+    nighttimeThemeList.value = localStorage[nighttimeThemeKey];
+});
 
 // Set the value to match the one in localStorage.
 alarmIntervalInput.value = parseInt(localStorage[checkTimeIntervalKey]);
 sunriseInput.value = localStorage[sunriseTimeKey];
 sunsetInput.value = localStorage[sunsetTimeKey];
 
+// Manually check the time and change the theme if appropriate.
+checkTimeBtn.addEventListener("click", function(event) {
+    console.log("test");
+    checkTime();
+});
+
+// Change the sunrise time.
 sunriseInput.addEventListener("input", function(event) {
-    // Change the sunrise time.
 
     localStorage[sunriseTimeKey] = sunriseInput.value;
     console.log("Set the sunrise time to " + sunriseInput.value);
 });
 
-
+// Change the sunset time.
 sunsetInput.addEventListener("input", function(event) {
-    // Change the sunset time.
 
     localStorage[sunsetTimeKey] = sunsetInput.value;
     console.log("Set the sunset time to " + sunsetInput.value);
 });
 
+// Change the alarm interval time.
 alarmIntervalInput.addEventListener("input", function(event) {
-    // Change the alarm interval time.
-    localStorage[checkTimeIntervalKey] = parseInt(alarmIntervalInput.value);
 
+    localStorage[checkTimeIntervalKey] = parseInt(alarmIntervalInput.value);
     updateCheckTime(localStorage[checkTimeIntervalKey]);
 });
 
-document.getElementById("check-time-btn").addEventListener("click", function(event) {
-    checkTime();
-});
+// Set the daytime theme.
+daytimeThemeList.addEventListener('change', 
+    function() {
+        localStorage[daytimeThemeKey] = this.value;
+    }
+);
 
-document.getElementById("radio-default").addEventListener("click", function(event) {
-    // Set the current theme to the default theme.
-    resetTheme();
-});
+// Set the nighttime theme.
+nighttimeThemeList.addEventListener('change', 
+    function() {
+        localStorage[nighttimeThemeKey] = this.value;
+    }
+);
 
-document.getElementById("radio-dark").addEventListener("click", function(event) {
-    // Set the current theme to the dark theme.
-    setTheme('dark');
-});
+// Reset all settings to their default.
+resetDefaultBtn.addEventListener("click", 
+    function(event) {
+        localStorage[checkTimeIntervalKey] = DEFAULT_CHECK_TIME_INTERVAL;
+        localStorage[sunriseTimeKey] = DEFAULT_SUNRISE_TIME;
+        localStorage[sunsetTimeKey] = DEFAULT_SUNSET_TIME;
+        localStorage[daytimeThemeKey] = DEFAULT_DAYTIME_THEME;
+        localStorage[nighttimeThemeKey] = DEFAULT_NIGHTTIME_THEME;
 
-document.getElementById("radio-light").addEventListener("click", function(event) {
-    // Set the current theme to the light theme.
-    setTheme('light');
-});
+        alarmIntervalInput.value = DEFAULT_CHECK_TIME_INTERVAL;
+        sunriseInput.value = DEFAULT_SUNRISE_TIME;
+        sunsetInput.value = DEFAULT_SUNSET_TIME;
+        daytimeThemeList.value = DEFAULT_DAYTIME_THEME;
+        nighttimeThemeList.value = DEFAULT_NIGHTTIME_THEME;
+    }
+);
 
-document.getElementById("reset-default-btn").addEventListener("click", function(event) {
-    // Reset all settings to their default.
-    localStorage[checkTimeIntervalKey] = 5;
-    localStorage[sunriseTimeKey] = "08:00";
-    localStorage[sunsetTimeKey] = "20:00";
-    resetTheme();
-
-    document.getElementById("radio-default").checked = false;
-    document.getElementById("radio-dark").checked = false;
-    document.getElementById("radio-light").checked = false;
-
-});
