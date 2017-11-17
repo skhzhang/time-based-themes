68b6867f7ca866e2a7d25b9d87ecefa0ddf1f9b0
Simon Zhang <simonkhzhang@gmail.com>
2017-11-17 18:29:20 -0500

    - Clean up and move files.

--- js/background.js
+++ js/background.js
@@ -3,4 +3,6 @@ checkTime();
 
 // Set up an alarm to check this regularly.
 browser.alarms.onAlarm.addListener(checkTime);
-browser.alarms.create('checkTime', {periodInMinutes: parseInt(localStorage[checkTimeIntervalKey])});
+browser.alarms.create('checkTime', 
+  {periodInMinutes: parseInt(localStorage[checkTimeIntervalKey])}
+);
