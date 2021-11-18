c0450bcd3530beeda4a8e5bac37ceb998758d16e
Simon Zhang <simonkhzhang@gmail.com>
2021-11-17 21:54:17 -0500

    Open settings page automatically when extension is installed

--- js/background.js
+++ js/background.js
@@ -1,3 +1,14 @@
 'use strict';
 
 init();
+
+browser.runtime.onInstalled.addListener(async ({reason, previousVersion}) => {
+  if (DEBUG_MODE)
+    console.log("automaticDark DEBUG: 0 - Installed - Reason: " + reason + ", Previous Version: " + previousVersion + ".");
+
+  if (reason === 'install') {
+    if (DEBUG_MODE)
+      console.log("automaticDark DEBUG: 0 - Open the options page.");
+    browser.runtime.openOptionsPage();
+  }
+});
